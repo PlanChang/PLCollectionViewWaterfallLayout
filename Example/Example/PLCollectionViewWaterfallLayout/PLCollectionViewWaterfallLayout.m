b3faddd85569baf1369697478e696481c0205207
plan <changshitong@gaoshou.inc.com>
2015-09-16 02:50:17 +0800

    Create PLCollectionViewWaterfallLayout files

---
+++ Example/Example/PLCollectionViewWaterfallLayout/PLCollectionViewWaterfallLayout.m
@@ -0,0 +1,13 @@
+//
+//  PLCollectionViewWaterfallLayout.m
+//  Example
+//
+//  Created by Plan on 15/9/16.
+//  Copyright (c) 2015年 PLAN. All rights reserved.
+//
+
+#import "PLCollectionViewWaterfallLayout.h"
+
+@implementation PLCollectionViewWaterfallLayout
+
+@end
