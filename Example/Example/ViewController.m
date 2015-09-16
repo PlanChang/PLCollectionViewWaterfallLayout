5a1c98558a9ac512ffa69280aad0c50ca75942e8
plan <changshitong@gaoshou.inc.com>
2015-09-17 03:57:03 +0800

    Basic form

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -9,14 +9,67 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UICollectionView *collectionView;
 @end
 
 @implementation ViewController
 
+- (void)dealloc
+{
+    _collectionView = nil;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self.view addSubview:self.collectionView];
+}
+
+- (UICollectionView *)collectionView
+{
+    if (!_collectionView) {
+        PLCollectionViewWaterfallLayout *layout = [[PLCollectionViewWaterfallLayout alloc] init];
+        layout.minimumRowSpacing = 10.0f;
+        layout.minimumInteritemSpacing = 10.0f;
+        layout.rowCount = 4;
+        layout.sectionInset = UIEdgeInsetsMake(10.0f, 10.0f, 10.0f, 10.0f);
+        
+        CGRect rect = CGRectMake(0.0f, 0.0f, CGRectGetWidth(self.view.bounds), 300.0f);
+        _collectionView = [[UICollectionView alloc] initWithFrame:rect collectionViewLayout:layout];
+        _collectionView.center = self.view.center;
+        _collectionView.delegate = self;
+        _collectionView.dataSource = self;
+        
+        
+        [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cell"];
+    }
+    return _collectionView;
+}
+
+#pragma mark - UICollectionView DataSource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor redColor];
+    return cell;
+}
+
+#pragma mark - PLCollectionViewDelegateWaterfallLayout
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section{
+    CGSize size={0.0f, 0.0f};
+    return size;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(arc4random()%50+50.0f,50.0f);
 }
 
 - (void)didReceiveMemoryWarning {
